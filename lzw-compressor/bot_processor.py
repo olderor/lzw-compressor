b57237eee6c109c1f740dfc0e840fc86b26b396a
Yevchenko Bohdan <98body@gmail.com>
2017-11-04 00:12:28 +0200

    add filtering banned users & minor fixes

--- lzw-compressor/bot_processor.py
+++ lzw-compressor/bot_processor.py
@@ -5,8 +5,11 @@ from page_compressor import PageCompressor
 from compressor_processor import *
 from file_manager import create_directory_if_not_exists
 
+
 TOKEN = "450503576:AAGBzxaZDaPyrP4Fuu-3J8_I47raOpsavfM"
 ADMIN_CHAT_ID = 152550720
+BANNED_USERS = [('Dmytro', 'Nazarenko')]
+
 
 bot = telebot.TeleBot(TOKEN)
 logger = telebot.logger
@@ -14,58 +17,78 @@ telebot.logger.setLevel(logging.DEBUG)
 compressor = CompressorProcessor()
 
 
-def on_get_message(message):
-    try:
-        username = "usernameNone"
-        if message.from_user.username:
-            username = message.from_user.username
-        first_name = "first_nameNone"
-        if message.from_user.first_name:
-            first_name = message.from_user.first_name
-        last_name = "last_nameNone"
-        if message.from_user.last_name:
-            last_name = message.from_user.last_name
+def messages_filter(handler):
+    def handle(message):
+        for user in BANNED_USERS:
+            if message.from_user.first_name == user[0] and \
+                            message.from_user.last_name == user[1]:
+                return
+        handler(message)
 
-        bot.send_message(ADMIN_CHAT_ID,
+    return handle
+
+
+def message_logger(handler):
+    def handle(message):
+        try:
+            username = "usernameNone"
+            if message.from_user.username:
+                username = message.from_user.username
+            first_name = "first_nameNone"
+            if message.from_user.first_name:
+                first_name = message.from_user.first_name
+            last_name = "last_nameNone"
+            if message.from_user.last_name:
+                last_name = message.from_user.last_name
+
+            bot.send_message(ADMIN_CHAT_ID,
                          "Got message from " + username +
                          " (" + first_name + " " + last_name + ").")
-        bot.forward_message(ADMIN_CHAT_ID, message.chat.id, message.message_id)
-    except Exception as e:
-        logger.error(str(e))
+            bot.forward_message(ADMIN_CHAT_ID, message.chat.id, message.message_id)
+        except Exception as e:
+            logger.error(str(e))
+        handler(message)
+
+    return handle
 
 
 @bot.message_handler(commands=["compress"])
+@message_logger
+@messages_filter
 def select_compress_mode(message):
-    on_get_message(message)
     compressor.set_mode(message.chat.id, CompressingMode.COMPRESS)
     bot.send_message(message.chat.id, "Selected compress mode. Now send a file to compress.")
 
 
 @bot.message_handler(commands=["decompress"])
+@message_logger
+@messages_filter
 def select_compress_mode(message):
-    on_get_message(message)
     compressor.set_mode(message.chat.id, CompressingMode.DECOMPRESS)
     bot.send_message(message.chat.id, "Selected decompress mode. Now send a compressed file.")
 
 
 @bot.message_handler(commands=["compare"])
+@message_logger
+@messages_filter
 def select_compress_mode(message):
-    on_get_message(message)
     compressor.set_mode(message.chat.id, CompressingMode.COMPARE)
     bot.send_message(message.chat.id, "Selected comparing mode. Now send two files to compare its content.")
 
 
 @bot.message_handler(commands=["help"])
+@message_logger
+@messages_filter
 def help(message):
-    on_get_message(message)
     bot.send_message(message.chat.id,
 """Select a mode: /compress /decompress or /compare and send me a file.\n
 Or just send me a link and I will compress all .txt files listed on that page.""")
 
 
 @bot.message_handler(content_types=["text"])
+@message_logger
+@messages_filter
 def undefined_text(message):
-    on_get_message(message)
     links = Parser.find_urls(message.text)
     if len(links) == 0:
         bot.reply_to(message,
@@ -81,8 +104,9 @@ Or just send me a link and I will compress all .txt files listed on that page.""
 
 
 @bot.message_handler(content_types=['document'])
+@message_logger
+@messages_filter
 def handle_docs(message):
-    on_get_message(message)
     try:
         chat_id = message.chat.id
         file_info = bot.get_file(message.document.file_id)
@@ -102,9 +126,10 @@ def handle_docs(message):
         bot.reply_to(message, e)
 
 
-@bot.message_handler(content_types=['photo'], func=lambda message: not message.from_user.is_bot)
+@bot.message_handler(content_types=['photo'])
+@message_logger
+@messages_filter
 def handle_photo(message):
-    on_get_message(message)
     try:
         chat_id = message.chat.id
         directory = "./data/" + str(chat_id) + "/photo/"
@@ -127,8 +152,9 @@ def handle_photo(message):
 
 
 @bot.message_handler(content_types=['video'])
+@message_logger
+@messages_filter
 def handle_video(message):
-    on_get_message(message)
     try:
         chat_id = message.chat.id
         file_info = bot.get_file(message.video.file_id)
@@ -150,8 +176,9 @@ def handle_video(message):
 
 
 @bot.message_handler(content_types=['audio'])
+@message_logger
+@messages_filter
 def handle_audio(message):
-    on_get_message(message)
     try:
         chat_id = message.chat.id
         file_info = bot.get_file(message.audio.file_id)
@@ -173,8 +200,9 @@ def handle_audio(message):
 
 
 @bot.message_handler(content_types=['sticker'])
+@message_logger
+@messages_filter
 def handle_sticker(message):
-    on_get_message(message)
     try:
         chat_id = message.chat.id
         file_info = bot.get_file(message.sticker.file_id)
